d949beb9a14f334d21b084e23ef64c0b4bcbf3e4
Victor Mikhaylov <vic.m@mail.ru>
2019-06-17 22:15:31 +0300

    dz1: fix test

--- src/test/java/com/epam/lesson2/dz/application/IndexAppTest.java
+++ src/test/java/com/epam/lesson2/dz/application/IndexAppTest.java
@@ -1,8 +1,10 @@
 package com.epam.lesson2.dz.application;
 
+import com.epam.lesson2.dz.myDefinitions.MyBeanDefinitionReader;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +12,14 @@ public class IndexAppTest {
 
     @Test
     public void whenGetBeans_returnsBean() {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-conf.xml");
-        IndexApp indexApp = applicationContext.getBean("indexApp", IndexApp.class);
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application-conf.xml");
+        GenericApplicationContext ctx = new GenericApplicationContext();
+
+        MyBeanDefinitionReader bdr = new MyBeanDefinitionReader(ctx);
+        bdr.loadBeanDefinitions("virtual");
+        ctx.refresh();
+
+        IndexApp indexApp = ctx.getBean("indexApp", IndexApp.class);
         assertNotNull(indexApp);
         assertSame("Hello World", indexApp.getService().serve());
     }
